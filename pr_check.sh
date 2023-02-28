5c8ea30b7c8ee56316f15b34fd88c0cab4f8a63e
Chris Hambridge <chambrid@redhat.com>
2023-02-28 10:06:31 -0500

    Add pr_check.sh script.
    * Add script to create image, deploy clowder, create empty JUnit output

---
+++ pr_check.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# --------------------------------------------
+# Options that must be configured by app owner
+# --------------------------------------------
+export APP_NAME="ros"  # name of app-sre "application" folder this component lives in
+export COMPONENT_NAME="ros-ocp-backend"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+export COMPONENTS="ros-ocp-backend"
+export IMAGE="quay.io/cloudservices/ros-ocp-backend"
+export DOCKERFILE="Dockerfile"
+
+
+# Install bonfire repo/initialize
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+
+source $CICD_ROOT/build.sh
+
+export EXTRA_DEPLOY_ARGS="--set-parameter ros-ocp-backend/KRUIZE_IMAGE_TAG=latest"
+
+# Deploy to an ephemeral namespace for testing
+source $CICD_ROOT/deploy_ephemeral_env.sh
+
+mkdir -p $WORKSPACE/artifacts
+cat << EOF > ${WORKSPACE}/artifacts/junit-dummy.xml
+<testsuite tests="1">
+    <testcase classname="dummy" name="dummytest"/>
+</testsuite>
+EOF
