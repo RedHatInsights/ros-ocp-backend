4e63b0ec1cd797bc5a0e179db0210c7087cb0688
Suraj Patil <patilsuraj767@gmail.com>
2023-03-01 13:08:21 +0530

    Soft enum on WorkloadType column

--- internal/model/workload.go
+++ internal/model/workload.go
@@ -4,16 +4,17 @@ import (
 	"time"
 
 	"github.com/lib/pq"
+	"github.com/redhatinsights/ros-ocp-backend/internal/types/workload"
 )
 
 type Workload struct {
 	ID              uint `gorm:"primaryKey;not null;autoIncrement"`
 	ClusterID       uint
 	Cluster         Cluster
-	ExperimentName  string         `gorm:"type:text"`
-	Namespace       string         `gorm:"type:text"`
-	WorkloadType    string         `gorm:"type:text"`
-	WorkloadName    string         `gorm:"type:text"`
-	Containers      pq.StringArray `gorm:"type:text[];index:,type:gin"`
+	ExperimentName  string                `gorm:"type:text"`
+	Namespace       string                `gorm:"type:text"`
+	WorkloadType    workload.WorkloadType `gorm:"type:text"`
+	WorkloadName    string                `gorm:"type:text"`
+	Containers      pq.StringArray        `gorm:"type:text[];index:,type:gin"`
 	MetricsUploadAt time.Time
 }
