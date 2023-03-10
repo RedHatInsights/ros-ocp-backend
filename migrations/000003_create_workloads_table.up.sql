323e2a359b3f75dc6210962ffbc09d4a3b0cfa24
Suraj Patil <patilsuraj767@gmail.com>
2023-03-10 10:41:05 +0530

    Added workload_type enum in DB

--- migrations/000003_create_workloads_table.up.sql
+++ migrations/000003_create_workloads_table.up.sql
@@ -1,9 +1,11 @@
+CREATE TYPE workloadtype AS ENUM ('deployment', 'deploymentconfig', 'replicaset', 'replicationcontroller', 'statefulsets');
+
 CREATE TABLE IF NOT EXISTS workloads(
    id BIGSERIAL PRIMARY KEY,
    cluster_id BIGINT NOT NULL,
    experiment_name TEXT NOT NULL,
    namespace TEXT NOT NULL,
-   workload_type TEXT NOT NULL,
+   workload_type workloadtype NOT NULL,
    workload_name TEXT NOT NULL,
    containers TEXT[] NOT NULL,
    metrics_upload_at TIMESTAMP WITH TIME ZONE
