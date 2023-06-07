7b91f6f40f7b2e6cb918420e1e252720b4235d0d
Suraj Patil <31805557+patilsuraj767@users.noreply.github.com>
2023-06-07 14:54:45 +0530

    Fixes #RHIROS-1126 - Handle daily data from operator (#81)

--- internal/model/workload_metrics.go
+++ internal/model/workload_metrics.go
@@ -32,3 +32,10 @@ func (w *WorkloadMetrics) CreateWorkloadMetrics() error {
 
 	return nil
 }
+
+func GetWorkloadMetricsForTimestamp(experiment_name string, interval_end time.Time) (WorkloadMetrics, error) {
+	db := database.GetDB()
+	var workload_metrics WorkloadMetrics
+	err := db.Table("workload_metrics").Joins("JOIN workloads ON workloads.experiment_name = ? AND workload_metrics.interval_end = ?", experiment_name, interval_end).Scan(&workload_metrics).Error
+	return workload_metrics, err
+}
