7f58f002fccadd29930a759539b61dd0ce201cdc
Suraj Patil <patilsuraj767@gmail.com>
2023-06-22 17:43:26 +0530

    Refs #RHIROS-1194 - Permission should be matched against cluster_uuid

--- internal/model/recommendation_set.go
+++ internal/model/recommendation_set.go
@@ -116,10 +116,10 @@ func add_rbac_filter(query *gorm.DB, user_permissions map[string][]string) {
 					query.Where("workloads.namespace IN (?)", project_permissions)
 					return
 				} else if utils.StringInSlice("*", project_permissions) {
-					query.Where("clusters.cluster_alias IN (?)", cluster_permissions)
+					query.Where("clusters.cluster_uuid IN (?)", cluster_permissions)
 					return
 				} else {
-					query.Where("clusters.cluster_alias IN (?)", cluster_permissions)
+					query.Where("clusters.cluster_uuid IN (?)", cluster_permissions)
 					query.Where("workloads.namespace IN (?)", project_permissions)
 					return
 				}
@@ -131,7 +131,7 @@ func add_rbac_filter(query *gorm.DB, user_permissions map[string][]string) {
 		if cluster_permissions, ok := user_permissions["openshift.cluster"]; ok {
 			if _, ok := user_permissions["openshift.project"]; !ok {
 				if !utils.StringInSlice("*", cluster_permissions) {
-					query.Where("clusters.cluster_alias IN (?)", cluster_permissions)
+					query.Where("clusters.cluster_uuid IN (?)", cluster_permissions)
 					return
 				}
 			}
