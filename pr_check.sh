7ca900f35b89772c6b886170a683580298955215
Suraj Patil <31805557+patilsuraj767@users.noreply.github.com>
2023-10-31 09:46:25 +0530

    Adding kruize component name in pr_check (#123)
    * Adding kruize component name in pr_check
    
    * Refs #RHCLOUD-28455 - override component_name before smoke tests
    
    (cherry picked from commit d1688492ab7bbe6528982d60445946e766153c32)
    
    ---------
    
    Co-authored-by: kgaikwad <kavitagaikwad103@gmail.com>

--- pr_check.sh
+++ pr_check.sh
@@ -4,7 +4,7 @@
 # Options that must be configured by app owner
 # --------------------------------------------
 export APP_NAME="ros"  # name of app-sre "application" folder this component lives in
-export COMPONENT_NAME="ros-ocp-backend"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+export COMPONENT_NAME="kruize ros-ocp-backend"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
 export COMPONENTS="kruize ros-ocp-backend"
 export IMAGE="quay.io/cloudservices/ros-ocp-backend"
 export DOCKERFILE="Dockerfile"
@@ -24,6 +24,7 @@ source $CICD_ROOT/build.sh
 source $CICD_ROOT/deploy_ephemeral_env.sh
 
 # Run iqe-ros-ocp smoke tests with ClowdJobInvocation
+export COMPONENT_NAME="ros-ocp-backend"
 source $CICD_ROOT/cji_smoke_test.sh
 
 # This will add the Ibutsu URL and test run IDs as a git check on PRs.
