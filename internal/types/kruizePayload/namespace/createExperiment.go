473ab8c86085a8e433038d32765d6bb25486c79d
saltgen <6542058+saltgen@users.noreply.github.com>
2025-11-14 15:21:23 +0530

    RHINENG-20010 Create the structs for namespace recommendations

---
+++ internal/types/kruizePayload/namespace/createExperiment.go
@@ -0,0 +1,18 @@
+package namespace
+
+import (
+	kruizePayload "github.com/redhatinsights/ros-ocp-backend/internal/types/kruizePayload"
+)
+
+//nolint:unused
+type createNamespaceExperiment struct {
+	Version                 string                               `json:"version"`
+	Experiment_name         string                               `json:"experiment_name"`
+	Cluster_name            string                               `json:"cluster_name"`
+	Performance_profile     string                               `json:"performance_profile"`
+	Mode                    string                               `json:"mode"`
+	Target_cluster          string                               `json:"target_cluster"`
+	Kubernetes_objects      []NamespaceKubernetesObject          `json:"kubernetes_objects"`
+	Trial_settings          kruizePayload.TrialSettings          `json:"trial_settings"`
+	Recommendation_settings kruizePayload.RecommendationSettings `json:"recommendation_settings"`
+}
