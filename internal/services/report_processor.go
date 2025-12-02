06ef75dd834bee2310ceaf279676bbb8c9918970
saltgen <6542058+saltgen@users.noreply.github.com>
2025-12-02 16:38:59 +0530

    RHINENG-22041 Namespace CSV Handling
    - Conditional handling for Namespace CSV - Environment variable to toggle namespace
    recommendations requests - Prometheus metric to capture invalid namespace CSVs from Cost

--- internal/services/report_processor.go
+++ internal/services/report_processor.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"encoding/json"
+	"strings"
 	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
@@ -65,6 +66,11 @@ func ProcessReport(msg *kafka.Message, _ *kafka.Consumer) {
 	}
 
 	for _, file := range kafkaMsg.Files {
+		if strings.Contains(file, "namespace") {
+			if cfg.DisableNamespaceRecommendation {
+				continue
+			}
+		}
 		data, err := utils.ReadCSVFromUrl(file)
 		if err != nil {
 			invalidCSV.Inc()
